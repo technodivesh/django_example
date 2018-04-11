6d6cb13f00db87fd96f46aaddab646e8dd4614b8
Divesh <divesh.chandolia@icreon.com>
2018-04-11 17:54:37 +0530

    first template page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question, Choice
+
 # Create your views here.
 def index(request):
 
-	return HttpResponse("Hello")
+	question_list = Question.objects.all()
+
+	context = {
+		'question_list':question_list
+	}
+	return render(request, 'polls/index.html', context )
+	# return HttpResponse("Hello")
+
+
+def detail(request, question_id):
+	return HttpResponse("Detail page")
+
+
+def vote(request, question_id):
+	return HttpResponse("Vote page %s" % question_id )
+
+
+def result(request, question_id):
+	return HttpResponse("Result page %s" % question_id )
