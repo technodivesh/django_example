a8dcf75542b3c0d8b4e94bded55e295da9599c2c
Divesh <divesh.chandolia@icreon.com>
2018-04-26 11:13:07 +0530

    Admin: Adding Choice option in Add Question page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,6 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from . models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 
